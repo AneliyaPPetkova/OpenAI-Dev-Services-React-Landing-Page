8d9a0de16c66ea5cdf96b0338819469af3730248
AneliyaPPetkova <AneliyaP.Petkova@gmail.com>
2023-03-09 14:55:27 +0200

    Replace info subsection on the What is GPT3 section with a Solution component. Fix style issues on both desktop and mobile.

--- src/containers/aboutGPT3/AboutGPT3.tsx
+++ src/containers/aboutGPT3/AboutGPT3.tsx
@@ -2,23 +2,29 @@ import { Solution } from "../../components";
 import solutions from "../../data/solutions";
 import { SolutionItem } from "../../data/types";
 
+const whatIsGPT3: SolutionItem = {
+  id: 1,
+  title: `What is GPT-3`,
+  info: `We so opinion friends me message as delight. Whole front do of plate
+    heard oh ought. His defective nor convinced residence own. Connection
+    has put impossible own apartments boisterous. At jointure ladyship an
+    insisted so humanity he. Friendly bachelor entrance to on by.`,
+};
+
 const AboutGPT3 = () => {
   return (
-    <div id="wgpt3" className="about-gpt3 bg__gradient--secondary section__margin section__padding">
+    <div
+      id="wgpt3"
+      className="about-gpt3 bg__gradient--secondary section__margin section__padding"
+    >
       <div className="about-gpt3__info">
-        <div className="about-gpt3__info-title text--white">
-          <h3>What is GPT-3</h3>
-        </div>
-        <div className="about-gpt3__info-text text--blue">
-          We so opinion friends me message as delight. Whole front do of plate
-          heard oh ought. His defective nor convinced residence own. Connection
-          has put impossible own apartments boisterous. At jointure ladyship an
-          insisted so humanity he. Friendly bachelor entrance to on by.
-        </div>
+        <Solution solution={whatIsGPT3} />;
       </div>
       <div className="about-gpt3__title-container">
         <div className="about-gpt3__title">
-          <h2 className="text--gradient">The possibilities are beyond your imagination</h2>
+          <h2 className="text--gradient">
+            The possibilities are beyond your imagination
+          </h2>
         </div>
         <div className="about-gpt3__link text--orange">Explore The Library</div>
       </div>
