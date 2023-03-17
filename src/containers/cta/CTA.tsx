b87fe46b101132186ef94e6d89a5f072c33b9cec
AneliyaPPetkova <AneliyaP.Petkova@gmail.com>
2023-03-17 19:07:34 +0200

    Implement the CTA container component and styles

--- src/containers/cta/CTA.tsx
+++ src/containers/cta/CTA.tsx
@@ -1,7 +1,17 @@
 const CTA = () => {
   return (
-    <div id="cta" className="cta section__padding">
-      CTA
+    <div id="cta" className="cta section__margin">
+      <div className="cta__title">
+        <div className="cta__strapline text--light-black">
+          Request Early Access to Get Started
+        </div>
+        <h3 className="text--black">
+          Register today & start exploring the endless possiblities.
+        </h3>
+      </div>
+      <div className="cta__link text--white">
+        <a href="#">Get Started</a>
+      </div>
     </div>
   );
 };
