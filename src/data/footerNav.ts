3c5d87db42ad3bc82a1fe83580aa1ee58857c3ae
AneliyaPPetkova <AneliyaP.Petkova@gmail.com>
2023-04-05 11:27:33 +0300

    Add data for the footer navigation.

---
+++ src/data/footerNav.ts
@@ -0,0 +1,72 @@
+const footerNav = [
+  {
+    id: 1,
+    title: "Links",
+    items: [
+      {
+        id: 11,
+        title: "Overons",
+        link: "#",
+      },
+      {
+        id: 12,
+        title: "Social Media",
+        link: "#",
+      },
+      {
+        id: 13,
+        title: "Counters",
+        link: "#",
+      },
+      {
+        id: 14,
+        title: "Contact",
+        link: "#",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Company",
+    items: [
+      {
+        id: 21,
+        title: "Terms & Conditions",
+        link: "#",
+      },
+      {
+        id: 22,
+        title: "Privacy Policy",
+        link: "#",
+      },
+      {
+        id: 23,
+        title: "Contact",
+        link: "#",
+      }
+    ],
+  },
+  {
+    id: 3,
+    title: "Get in touch",
+    items: [
+      {
+        id: 31,
+        title: "Crechterwoord K12 182 DK Alknjkcb",
+        link: "#",
+      },
+      {
+        id: 32,
+        title: "085-132567",
+        link: "#",
+      },
+      {
+        id: 33,
+        title: "info@payme.net",
+        link: "#",
+      }
+    ],
+  },
+];
+
+export default footerNav;
