05e650166db3073eb9447ece49cead27c9d71cd2
AneliyaPPetkova <AneliyaP.Petkova@gmail.com>
2023-04-10 13:24:23 +0300

    Fix style issues in the Possibilities container component.

--- src/containers/possibilities/Possibilities.tsx
+++ src/containers/possibilities/Possibilities.tsx
@@ -2,7 +2,7 @@ import possibilities from "../../assets/possibilities.png";
 
 const Possibilities = () => {
   return (
-    <div id="possibilities" className="possibilities section__padding">
+    <div id="possibilities" className="possibilities">
       <div className="possibilities__image">
         <img
           src={possibilities}
@@ -10,11 +10,11 @@ const Possibilities = () => {
         />
       </div>
       <div className="possibilities__content">
-        <div className="possibilities__strapline text--light-blue">Join the waiting list</div>
-        <div className="possibilities__title text--gradient">
-          <h2>The possibilities are beyond your imagination</h2>
+        <div className="possibilities__strapline text--normal text--light-blue">Join the waiting list</div>
+        <div className="possibilities__title">
+          <h2 className="text--subtitle text--gradient">The possibilities are beyond your imagination</h2>
         </div>
-        <div className="possibilities__text text--blue">
+        <div className="possibilities__text text--normal text--blue">
           Welcome to a world of infinite possibilities. With GPT-3 from OpenAI,
           there's no limit to what you can achieve. From writing to design,
           automation to education, GPT-3 is transforming the way we live, work,
@@ -24,7 +24,7 @@ const Possibilities = () => {
           create compelling content, or solve complex problems, GPT-3 can help
           you achieve your goals. Are you ready to explore the endless potential of GPT-3?
         </div>
-        <div className="possibilities__link text--orange">
+        <div className="possibilities__link text--normal text--orange">
           Request Early Access to Get Started
         </div>
       </div>
