308b94878d0838bc2dd13a57286006ac161a5afe
AneliyaPPetkova <AneliyaP.Petkova@gmail.com>
2023-04-28 16:47:46 +0300

    Fix issue with blog post images not loading after build and deploy to Github Pages.

--- src/data/blog.ts
+++ src/data/blog.ts
@@ -1,4 +1,9 @@
 import { Article, NavList } from "./types";
+import blogImg1 from "../assets/blog-1.png";
+import blogImg2 from "../assets/blog-2.png";
+import blogImg3 from "../assets/blog-3.png";
+import blogImg4 from "../assets/blog-4.png";
+import blogImg5 from "../assets/blog-5.png";
 
 const blog: Article[] = [
   {
@@ -6,40 +11,40 @@ const blog: Article[] = [
     title: "GPT-3 and Open  AI is the future. Let us exlore how it is?",
     link: "#",
     publishedAt: "Sep 26, 2021",
-    imageUrl: "src/assets/blog-1.png",
-    isFeatured: true
+    imageUrl: blogImg1,
+    isFeatured: true,
   },
   {
     id: 2,
     title: "GPT-3 and Open  AI is the future. Let us exlore how it is?",
     link: "#",
     publishedAt: "Sep 26, 2021",
-    imageUrl: "src/assets/blog-2.png",
-    isFeatured: false
+    imageUrl: blogImg2,
+    isFeatured: false,
   },
   {
     id: 3,
     title: "GPT-3 and Open  AI is the future. Let us exlore how it is?",
     link: "#",
     publishedAt: "Sep 26, 2021",
-    imageUrl: "src/assets/blog-3.png",
-    isFeatured: false
+    imageUrl: blogImg3,
+    isFeatured: false,
   },
   {
     id: 4,
     title: "GPT-3 and Open  AI is the future. Let us exlore how it is?",
     link: "#",
     publishedAt: "Sep 26, 2021",
-    imageUrl: "src/assets/blog-4.png",
-    isFeatured: false
+    imageUrl: blogImg4,
+    isFeatured: false,
   },
   {
     id: 5,
     title: "GPT-3 and Open  AI is the future. Let us exlore how it is?",
     link: "#",
     publishedAt: "Sep 26, 2021",
-    imageUrl: "src/assets/blog-5.png",
-    isFeatured: false
+    imageUrl: blogImg5,
+    isFeatured: false,
   },
 ];
 
