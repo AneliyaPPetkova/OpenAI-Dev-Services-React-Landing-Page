cb71fcadab906c67ba69af32322e544ccf6babc2
AneliyaPPetkova <AneliyaP.Petkova@gmail.com>
2023-05-31 15:35:45 +0300

    Configure Vite dev server to run on port 3000.

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/OpenAI-Dev-Services-React-Landing-Page/", // Needed for GitHub Pages deployment
+  server: {
+    port: 3000,
+  },
 })
